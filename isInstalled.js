edd86f90612be6bbaa37b9e4df34a253463fd8ae
Jiri Prokop <jiri-prokop@synaptiko.cz>
2013-10-19 12:45:08 +0200

    extension can detect it's installed

---
+++ isInstalled.js
@@ -0,0 +1,5 @@
+var installButton = document.getElementById('install-button');
+var extensionInstalled = document.getElementById('extension-installed');
+
+installButton.style.display = 'none';
+extensionInstalled.style.display = 'block';
